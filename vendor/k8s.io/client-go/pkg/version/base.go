e790b14563bd396293f3015857d7451808826ac7
Ilia Donchenko <iliad@me.com>
2018-10-02 12:08:08 +0300

    Update collection and indexes

--- vendor/k8s.io/client-go/pkg/version/base.go
+++ vendor/k8s.io/client-go/pkg/version/base.go
@@ -57,7 +57,7 @@ var (
 	// directory.  See also https://git-scm.com/docs/gitattributes
 	gitVersion   string = "v0.0.0-master+78700de"
 	gitCommit    string = "78700dec6369ba22221b72770783300f143df150" // sha1 from git, output of $(git rev-parse HEAD)
-	gitTreeState string = ""                                         // state of git tree, either "clean" or "dirty"
+	gitTreeState string = ""            // state of git tree, either "clean" or "dirty"
 
 	buildDate string = "1970-01-01T00:00:00Z" // build date in ISO8601 format, output of $(date -u +'%Y-%m-%dT%H:%M:%SZ')
 )
